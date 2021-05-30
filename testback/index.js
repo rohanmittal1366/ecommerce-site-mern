e9d273b0ef52ae70e7a9d7663aa1e4fb9993fbe3
rohan_mittal <rohanmittal1366@gmail.com>
2021-05-30 20:09:20 +0530

    add testing of middleware

--- testback/index.js
+++ testback/index.js
@@ -2,11 +2,25 @@ const express = require("express");
 
 const app = express();
 
-const port = 8000;
+const port = 3000;
 
 app.get("/", (req, res) => {
   return res.send("Home page");
 });
+
+const admin = (req, res) => {
+  return res.send("Admin dashboard ");
+};
+const isAdmin = (req, res, next) => {
+  console.log("Hii ia ma here");
+  next();
+};
+const isLoggedin = (req, res, next) => {
+  console.log("hello i am loggedin");
+  next();
+};
+
+app.get("/admin", isLoggedin, isAdmin, admin);
 
 app.get("/login", (req, res) => {
   return res.send("You are visiting login route");
