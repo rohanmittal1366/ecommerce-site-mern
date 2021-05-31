75e11e969617b4a969d2e875002581ac28ad619a
rohan_mittal <rohanmittal1366@gmail.com>
2021-05-31 20:26:31 +0530

    added user validation

--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -2,9 +2,17 @@ var express = require("express");
 
 var router = express.Router();
 
+const { check, validationResult } = require("express-validator");
+
 const { signout, signup } = require("../controllers/auth");
 
-router.post("/signup", signup);
+router.post(
+  "/signup",
+  [check("name", "name should be atleast 3 character").isLength({ min: 3 })],
+  [check("email", "email is required").isEmail()],
+  [check("password", "password must be strong").isStrongPassword()],
+  signup
+);
 
 router.get("/signout", signout);
 
