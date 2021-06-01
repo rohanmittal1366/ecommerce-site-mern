062048ce86580d30373bdccca1fddc8fd195f1ca
rohan_mittal <rohanmittal1366@gmail.com>
2021-06-02 04:37:48 +0530

    added bulk write operation in sold and stock count

--- projbackend/controllers/product.js
+++ projbackend/controllers/product.js
@@ -154,3 +154,23 @@ exports.getAllProducts = (req, res) => {
       res.json(products);
     });
 };
+
+exports.updateStock = (req, res, next) => {
+  let myOperations = req.body.order.products.map((prod) => {
+    return {
+      updateOne: {
+        filter: { _id: prod._id },
+        update: { $inc: { stock: -prod.count, sold: +prod.count } },
+      },
+    };
+  });
+
+  Product.bulkWrite(myOperations, {}, (err, product) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Bulk operation failed",
+      });
+    }
+    next();
+  });
+};
