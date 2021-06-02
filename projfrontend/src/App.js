e62774425707e16bb4206aff644f744e93ca0187
rohan_mittal <rohanmittal1366@gmail.com>
2021-06-02 19:30:23 +0530

    Reformated stylesheets

--- projfrontend/src/App.js
+++ projfrontend/src/App.js
@@ -1,11 +1,9 @@
-import "./App.css";
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>Hii</h1>
+    <div>
+      <h1>Hi</h1>
     </div>
   );
 }
-
-export default App;
---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles.css";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-white">Hello Front end </h1>
+    </div>
+  );
+}
+
+export default Home;
