08e28f4030c2799ac3ac27f8194c2037a883c33f
rohan_mittal <rohanmittal1366@gmail.com>
2021-06-03 23:12:43 +0530

    added form in signin and signup

--- projfrontend/src/user/Signin.js
+++ projfrontend/src/user/Signin.js
@@ -2,9 +2,28 @@ import React from "react";
 import Base from "../core/Base";
 
 const Signin = () => {
+  const signInForm = () => {
+    return (
+      <div className="row">
+        <div className="col-md-4 offset-sm-4 text-left">
+          <form>
+            <div className="form-group">
+              <label className="text-light">Email</label>
+              <input className="form-control" type="email" />
+            </div>
+            <div className="form-group">
+              <label className="text-light">Password</label>
+              <input className="form-control" type="password" />
+            </div>
+            <button className="btn btn-success btn-block">Submit</button>
+          </form>
+        </div>
+      </div>
+    );
+  };
   return (
     <Base title="Signin page " discription="A page for user to signin">
-      <h1>Signup</h1>
+      {signInForm()}
     </Base>
   );
 };
