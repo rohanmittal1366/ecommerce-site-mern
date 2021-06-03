44dd815cf1a20e156d99807320050c4d73213f0a
rohan_mittal <rohanmittal1366@gmail.com>
2021-06-04 00:31:44 +0530

    make signup component and connect with database

--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,8 +1,44 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
+import { signup } from "../auth/helper/index";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error) {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            error: "",
+            success: true,
+            password: "",
+          });
+        }
+      })
+      .catch((err) => console.log("Error in  signup"));
+  };
+
   const signUpForm = () => {
     return (
       <div className="row">
@@ -10,18 +46,67 @@ const Signup = () => {
           <form>
             <div className="form-group">
               <label className="text-light">Name</label>
-              <input className="form-control" type="text" />
+              <input
+                className="form-control"
+                onChange={handleChange("name")}
+                type="text"
+                value={name}
+              />
             </div>
             <div className="form-group">
               <label className="text-light">Email</label>
-              <input className="form-control" type="email" />
+              <input
+                className="form-control"
+                type="email"
+                onChange={handleChange("email")}
+                value={email}
+              />
             </div>
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password" />
+
+              <input
+                className="form-control"
+                type="password"
+                onChange={handleChange("password")}
+                value={password}
+              />
             </div>
-            <button className="btn btn-success btn-block">Submit</button>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
           </form>
+        </div>
+      </div>
+    );
+  };
+
+  const successMesssage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            New account was created successfully.{" "}
+            <Link to="/signin">Please Login here</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMesssage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
         </div>
       </div>
     );
@@ -29,7 +114,10 @@ const Signup = () => {
 
   return (
     <Base title="Sign up page" description="A page for user to sign up!">
+      {successMesssage()}
+      {errorMesssage()}
       {signUpForm()}
+      {/* <p className="text-white text-center">{JSON.stringify(values)}</p> */}
     </Base>
   );
 };
